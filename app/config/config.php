77da2b3459c292c01dcadbef87f31c66510d1a09
clarksye <burhanbasyri@gmail.com>
2021-05-31 01:49:21 -0700

    manage dummy file

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASEURL","https://yoursite.com/"); //baseurl
+
+// Database
+define("DB_HOST", "localhost");
+define("DB_USER", "user");
+define("DB_PASS", "pas");
+define("DB_NAME", "name");
--- app/init.php
+++ app/init.php
@@ -10,3 +10,6 @@ spl_autoload_register(function( $class ) {
     require_once __DIR__ . '/core/' . $class . '.php';
 });
 // <end/>
+
+// config
+require_once 'config/config.php';
